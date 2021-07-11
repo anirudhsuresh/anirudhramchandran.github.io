ee072b5cad59c642e920c46981ee2dd88008ba2d
Anirudh <aniramch@ucdavis.edu>
2021-07-10 17:46:19 -0700

    Updates

---
+++ precache-manifest.357adf66c51ab2ec2526671440decc3f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a04aefb395c3907443d4b16b12c57ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb40b63d978f8c915d2a",
+    "url": "/static/css/main.022eb82c.chunk.css"
+  },
+  {
+    "revision": "ea4c23db1757a4f0b504",
+    "url": "/static/js/2.c6a95f53.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.c6a95f53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb40b63d978f8c915d2a",
+    "url": "/static/js/main.c85a8f0f.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "e50a2a64f4816db17e933ebc467e6cbb",
+    "url": "/static/media/anirudh.e50a2a64.jpg"
+  },
+  {
+    "revision": "32f6b574eb11519020e24620a38bdf55",
+    "url": "/static/media/hashirshoaeb.32f6b574.png"
+  },
+  {
+    "revision": "505d310b8462c40ef6c1f705fc1f11db",
+    "url": "/static/media/resume.505d310b.pdf"
+  }
+]);
